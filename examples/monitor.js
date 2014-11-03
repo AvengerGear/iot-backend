33eab038270710a811f4dc72eaacd54f3370693f
Fred Chien <cfsghost@gmail.com>
2014-11-04 01:26:18 +0800

    initial architecture

---
+++ examples/monitor.js
@@ -0,0 +1,16 @@
+var IoTBackend = require('../');
+
+var backend = new IoTBackend({
+	username: 'fred@avengergear.com',
+	password: 'hello'
+});
+
+backend.on('ready', function(backend) {
+
+	console.log('Connected IoT Network');
+
+	var incoming = backend.bus('incoming');
+	incoming.hook('*', function(message) {
+		console.log(message);
+	});
+});
